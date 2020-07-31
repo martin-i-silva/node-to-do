7c188ce00bd42c39e86ac9aaedb18e0644769d72
Martin <martin.i.silva@gmail.com>
2020-07-31 15:54:12 -0300

    Primer commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## Aplicacion de comandos 
+
+Este es el ejercicio del curso
+
+Instalar los paquetes de node  
+
+```
+nmp install
+
+```
+
+
---
+++ app.js
@@ -0,0 +1,35 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors');
+const porHacer = require('./to-do/to-do')
+
+
+let comando = argv._[0];
+
+switch(comando){
+    case 'crear': 
+       let tarea =  porHacer.crear(argv.descripcion);
+       console.log(tarea);
+    break;
+    case 'listar':
+        let listado = porHacer.getListado()
+        
+        for (i of listado){
+        console.log('========To Do========'.green)
+        console.log(i.descripcion)
+        console.log('Estado: ', i.completado)
+        console.log('====================='.green)
+    }
+    break;
+    case 'actualizar':
+        let actualizado = porHacer.actualizar(argv.descripcion, argv.completado)
+        console.log(actualizado)
+    break;
+    case 'borrar':
+        let borrado = porHacer.borrar(argv.descripcion);
+        console.log(borrado)
+    break;
+    
+    default: console.log('Comando no reconocido') ;
+
+}
+
